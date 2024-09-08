0efb72d27418e04579478eddab461899a3915e5b
harshavardhangs <gsharshavardhan01@gmail.com>
2024-09-08 12:37:16 +0530

    newresumeupdated

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -8,7 +8,7 @@ import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import LinkedInDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/harshavardhangs_resume.pdf';
+import CV from '../../assets/hars_resume.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
